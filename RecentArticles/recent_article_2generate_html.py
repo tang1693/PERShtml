70506536e710e901468e7788eeb7d0f209b66a8a
Yang Tang <tang.1693@osu.edu>
2024-11-19 00:30:33 -0500

    fold abs

--- RecentArticles/recent_article_2generate_html.py
+++ RecentArticles/recent_article_2generate_html.py
@@ -72,8 +72,9 @@ for index, row in articles.iterrows():
     article_html += f'''    <div>
         Abstract: <span id="abstract-short-{index}" style="display: inline;">{row["Abstract"][:100]}...</span>
         <span id="abstract-full-{index}" style="display: none;">{row["Abstract"]}</span>
-        <a href="javascript:void(0);" id="toggle-abstract-{index}" onclick="toggleAbstract({index}); return false;" style="color: #1b5faa; text-decoration: none;">[Read more]</a>
+        <a href="#" id="toggle-abstract-{index}" onclick="toggleAbstract({index}); return false;" style="color: #1b5faa; text-decoration: none;">[Read more]</a>
     </div>\n'''
+
 
 
     
@@ -89,12 +90,10 @@ for index, row in articles.iterrows():
 # Add JavaScript and CSS for foldable functionality
 foldable_script = '''
 <script>
-    function toggleAbstract(index, event) {
-        event.preventDefault(); // Prevent the default behavior of the <a> tag
-
-        var shortAbstract = document.getElementById(`abstract-short-${index}`);
-        var fullAbstract = document.getElementById(`abstract-full-${index}`);
-        var toggleLink = document.getElementById(`toggle-abstract-${index}`);
+    function toggleAbstract(index) {
+        const shortAbstract = document.getElementById(`abstract-short-${index}`);
+        const fullAbstract = document.getElementById(`abstract-full-${index}`);
+        const toggleLink = document.getElementById(`toggle-abstract-${index}`);
         
         if (shortAbstract.style.display === "none") {
             shortAbstract.style.display = "inline";
